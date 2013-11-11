61d155f8f9bfc0f51e03313aa38c81b358f8f17e
N.Ardi <ardi@detik.com>
2013-11-11 22:50:10 +0700

    Update master.view

--- master/master/views.py
+++ master/master/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render_to_response
+from products.models import Items
 
 def home(request):
-    return render_to_response('page/home.html')
+    args = {}
+    pulsa = Items.objects.filter(item_type__name='pulsa').order_by('-mod_date')[:4]
+    args['all_pulsa'] = pulsa
+    args['total_pulsa'] = len(Items.objects.filter(item_type__name='pulsa'))
+    
+    return render_to_response('page/home.html', args)
