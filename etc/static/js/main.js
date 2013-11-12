d8c521e7f0ae7b4ddfa23088bdaa3487533744f4
N.Ardi <ardi@detik.com>
2013-11-12 21:49:24 +0700

    Update html dan javascript

--- etc/static/js/main.js
+++ etc/static/js/main.js
@@ -1,8 +1,20 @@
 $.fn.hargaIndonesia = function() {
     return this.each(function() {
         var self = $(this),
-            html = self.html()
+            html = self.html(),
+            arr = html.split(''),
+            rev = arr.reverse(),
+            ret = []
         ;
-        self.html(html + ',--');
+        
+        $.each(rev, function(index, obj) {
+            if (index != 0 && index % 3 == 0) {
+                ret.push('.');
+            }
+            
+            ret.push(obj);
+        })
+        
+        self.html(ret.reverse().join(''));
     })
 }
