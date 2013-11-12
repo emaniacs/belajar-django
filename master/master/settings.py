2969950811569f8eab7c78cad248627656118a84
N.Ardi <ardi@detik.com>
2013-11-12 17:04:19 +0700

    Update database path on settings.

--- master/master/settings.py
+++ master/master/settings.py
@@ -1,4 +1,7 @@
 # Django settings for master project.
+import os
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/ardi/git/myGit/terminal-cell/etc/db.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': THIS_DIR + '/../../etc/db.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
