d36a77da2acd3a5939c9d68f842ce9c3b3bea367
N.Ardi <ardi@detik.com>
2013-11-17 14:59:24 +0700

    Tambah filter untuk load menu.

---
+++ master/master/templatetags/my_menu.py
@@ -0,0 +1,17 @@
+from django import template
+from products.models import Menu
+
+register = template.Library()
+
+@register.filter(name="get_menu")
+def get_menu(name):
+    '''
+    '''
+        
+    menus = Menu.objects.filter(status=1).order_by('-nama')
+    for menu in menus:
+        if menu.nama.lower() == name:
+            menu.status = 'active'
+        else:
+            menu.status = ''
+    return menus
