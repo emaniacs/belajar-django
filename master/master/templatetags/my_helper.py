b74ccf873b5133f63f67f6c9061fd8197e052530
N.Ardi <ardi@detik.com>
2013-11-17 17:49:56 +0700

    Tambahkan method untuk ambil item by type, length.

--- master/master/templatetags/my_helper.py
+++ master/master/templatetags/my_helper.py
@@ -1,4 +1,5 @@
 from django import template
+from products.models import Items
 
 register = template.Library()
 
@@ -8,3 +9,26 @@ def create_range(val):
         create range'''
         
     return range(val)
+
+
+@register.filter(name='get_item_by_type')
+def get_item_by_type(tipe, skip=4):
+    
+    try:
+        items = Items.objects.filter(tipe_item__nama=tipe).order_by('-waktu_modif')[:skip]
+    except:
+        items = {}
+        
+    return items
+    
+@register.filter(name='get_total_item')
+def get_total_item(tipe=''):
+    if not tipe:
+        length = len(items = Items.objects.all())
+    else:
+        try:
+            length  = len(Items.objects.filter(tipe_item__nama=tipe))
+        except:
+            length = 0
+            
+    return length
